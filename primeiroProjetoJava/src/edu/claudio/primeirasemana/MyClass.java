8eaa5453cf0d84aa1b4e341ace6cf4acd8bd1e30
Claudi-oF <claudiofilhocf123@gmail.com>
2024-08-18 12:16:26 -0300

    variaveis java

--- primeiroProjetoJava/src/edu/claudio/primeirasemana/MyClass.java
+++ primeiroProjetoJava/src/edu/claudio/primeirasemana/MyClass.java
@@ -1,3 +1,4 @@
+package edu.claudio.primeirasemana;
 public class MyClass {
    
     public static void main (String [] args) {
---
+++ primeiroProjetoJava/src/edu/claudio/primeirasemana/VariablesTypes.java
@@ -0,0 +1,13 @@
+package edu.claudio.primeirasemana;
+public class VariablesTypes {
+    
+    public static void main(String [] args) {
+
+        int a = 1234;
+        short b = 500;
+        long int c = 499238126;
+        float d = 56.1231f;
+        double e = 8.213;
+
+    } 
+}
