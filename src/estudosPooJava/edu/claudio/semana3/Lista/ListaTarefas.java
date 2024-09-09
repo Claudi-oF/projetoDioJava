23e5db8209033d0582b722e6dc8b44c29cb9de9e
Claudi-oF <claudiofilhocf123@gmail.com>
2024-09-09 18:21:20 -0300

    resolvendo desafio ListaTarefa
    Signed-off-by: Claudi-oF <claudiofilhocf123@gmail.com>

--- src/estudosPooJava/edu/claudio/semana3/Lista/ListaTarefas.java
+++
@@ -1,7 +0,0 @@
-package estudosPooJava.edu.claudio.semana3.Lista;
-
-import java.util.ArrayList;
-
-public class ListaTarefas {
-    
-}
---
+++ src/estudosPooJava/edu/claudio/semana3/Lista/Tarefa.java
@@ -0,0 +1,16 @@
+package estudosPooJava.edu.claudio.semana3.Lista;
+
+public class Tarefa {
+    //atributo  
+    
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
